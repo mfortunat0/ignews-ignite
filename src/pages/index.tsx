750ac74a3a9385b7ef772842bf1b3aee8d1f9998
matheus <matheus.xmaz10@gmail.com>
2022-03-07 22:54:24 -0300

    pagina home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,25 @@
 import Head from "next/head";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>IG.news</title>
+        <title>Home - IG.news</title>
       </Head>
-      <div>
-        <h1>Hello</h1>
-      </div>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, Welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
